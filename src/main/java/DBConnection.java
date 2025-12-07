b7c71b0b1d4bc789570c75fbb31b81d2d1466ed6
Rel4w <hei.jonathan.4@gmail.com>
2025-12-07 12:38:02 +0300

    question 5

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
+    private String url;
+    private String user;
+    private String password;
+    private Connection connection;
+
+
+    public DBConnection(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+
+    public DBConnection() {
+        this(
+                "jdbc:postgresql://localhost:5432/product_management_db",
+                "product_manager_user",
+                "123456"
+        );
+    }
+
+
+    public Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        return connection;
+    }
+
+
+    public void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur lors de la fermeture de la connexion: " + e.getMessage());
+        }
+    }
+
+
+    public String getUrl() { return url; }
+    public String getUser() { return user; }
 }
